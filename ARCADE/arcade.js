47a20f27a37bcf3b780f1479d4c7e2651d9aacef
JonathanLozanoA <jonathanlozzano@gmail.com>
2024-12-31 10:09:37 -0500

    en js se add la 2da function, en html se modifica el h1 y h2, en css se cambia al background: #fafafa

--- ARCADE/arcade.js
+++ ARCADE/arcade.js
@@ -1,11 +1,47 @@
-function seleccionarPersonaje() {
-    alert("Hola")
-}
-function iniciarJuego(){
-    let botonTomy = document.querySelector("#botonTomy")
-    botonTomy.addEventListener("click", seleccionarPersonaje)
-}
 window.addEventListener("load", iniciarJuego)
+window.addEventListener("load", etapa2)
+
+// EVENTO DE SELECCIONAR PERSONAJE
+function iniciarJuego(){
+    let botonPersonaje = document.querySelector("#botonPersonaje")
+    botonPersonaje.addEventListener("click", seleccionarPersonaje)
+}
+function seleccionarPersonaje() {
+    let tuPersonaje = document.querySelector("#tuPersonaje")
+    let tuOponente = document.querySelector("#tuOponente")
+    if(botonTomy.checked) {
+        tuPersonaje.innerHTML = "😼Tomy"
+        tuOponente.innerHTML = "🐭Daly"
+    } else if(botonDaly.checked){
+        tuPersonaje.innerHTML = "🐭Daly"
+        tuOponente.innerHTML = "😼Tomy"
+    } else {
+        alert("Seleccione un personaje")
+    }
+}
+
+// EVENTO DE SELECCIONAR ATAQUE
+function etapa2(){
+    let botonAtaque = document.querySelector("#botonAtaque")
+    botonAtaque.addEventListener("click", seleccionarAtaque)
+}
+function seleccionarAtaque() {
+    if(laser.checked) {
+        alert("¡Rayo laser!")
+    } else if(cuchillo.checked) {
+        alert("¡Cuchillo de cocina!")
+    } else if(arco.checked) {
+        alert("¡Arco y flecha!")
+    } else if(bomba.checked) {
+        alert("¡TNT marca ACME!")
+    } else if(hacha.checked) {
+        alert("¡Hacha yo te elijo!")
+    } else if(patada.checked) {
+        alert("¡Patada voladora!")
+    } else {
+        alert("Seleccione un ataque")
+    }
+}
 
 
 
